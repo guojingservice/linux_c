aec1bdf2685aa712f963bbdff133609507182b74
guojing013214@163.com <guojing013214@163.com>
2015-03-26 10:31:54 +0800

    test fork

--- fork/fork.c
+++ fork/fork.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
+#include <unistd.h>
 int main()
 {
-	printf("HelloWorld\n");
 	
+	printf("Befor Fork..\n");
+	
+	pid_t pid = fork();
+	if(pid < 0)
+	{
+		printf("error occured when call fork()\n");
+	}
+	if(pid == 0 )
+	{
+		printf("I am the child\n");
+	}
+	else if(pid > 0)
+	{
+		printf("I am the parent!\n");
+	}
 	return 0;
 }
