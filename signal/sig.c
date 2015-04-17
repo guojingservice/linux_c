264631444bcc80c1b29c09a2b290ba3ab037a322
guojing013214@163.com <guojing013214@163.com>
2015-04-17 19:23:31 +0800

    signal test

--- signal/sig.c
+++ signal/sig.c
@@ -3,9 +3,43 @@
 #include <unistd.h>
 #include <signal.h>
 
+void sigroutine(int dunno)
+{
+	switch(dunno)
+	{
+		case SIGHUP:
+			{
+				printf("Receive Signal : SIGHUP\n");
+			}
+			break;
+		case SIGINT:
+			{
+				printf("Receive Signal : SIGINT\n");	
+			}
+			break;
+		case SIGQUIT:
+			{
+				printf("Receive Signal : SIGQUIT\n");
+			}
+			break;
+		case SIGILL:
+			{
+				printf("Receive Signal : SIGILL\n");
+			}
+			break;
+		default:
+			break;
+	}
+	return;
+}
+
 int main()
 {
-	
+	printf("my process id is :%d\n", getpid());
+	signal(SIGHUP, sigroutine);
+	signal(SIGINT, sigroutine);
+	signal(SIGQUIT, sigroutine);
+	for(;;);	
 	return 0;
 }
 
