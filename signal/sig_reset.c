28e4c9cf2cd37f368c8e4e549bea59d8bd84f568
guojing013214@163.com <guojing013214@163.com>
2015-04-20 11:02:35 +0800

    signal reset

---
+++ signal/sig_reset.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// process is running a signal process function when receive a same signal, the process will 
+// continue running, the new signal will be saved untill the last signal function is completed
+// and the appropriate signal process function will be executed
+
+int main()
+{
+	return 0;
+}
