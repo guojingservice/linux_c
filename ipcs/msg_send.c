810db76edf98ff7c75da1d16dc4081db706b2245
guojing013214@163.com <guojing013214@163.com>
2015-05-04 17:43:41 +0800

    send msg

--- ipcs/msg_send.c
+++ ipcs/msg_send.c
@@ -3,6 +3,38 @@
 #include <sys/types.h>
 #include <sys/msg.h>
 
+struct client_info{
+	long cmd;
+	char text[2048];
+};
+
+
+struct my_msgbuf{
+	long mtype;
+	long request_id;
+	struct client_info info;
+};
+
+int open_queue( key_t keyval )
+{
+	int qid;
+	if((qid = msgget( keyval, IPC_CREAT | 0660)) == -1)
+	{
+		return -1;
+	}
+	return qid;
+}
+
+int send_message( int qid, struct my_msgbuf *qbuf)
+{
+	int result, length;
+	length = sizeof(struct my_msgbuf) - sizeof(long);
+	if((result = msgsnd( qid, qbuf, length, 0)) == -1)
+	{
+		return -1;
+	}
+	return result;
+}
 
 int main(int argc, char **argv)
 {
