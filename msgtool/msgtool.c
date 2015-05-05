eab318271ce78c0b180134afb2913c7b98cc46b4
guojing013214@163.com <guojing013214@163.com>
2015-05-05 11:48:45 +0800

    msgtool for ipc msg

---
+++ msgtool/msgtool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
