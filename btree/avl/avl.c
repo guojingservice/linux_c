2c079b2e62659c9b8a602dd9e122ae2bf240874e
guojing013214@163.com <guojing013214@163.com>
2015-05-23 18:54:31 +0800

    avl truee

--- btree/avl/avl.c
+++ btree/avl/avl.c
@@ -1,12 +1,41 @@
+/*
+*	avl tree
+*	base functions
+*	
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+
 
 typedef int DATA_TYPE;
 
 typedef struct t_node{
-	DATA_TYPE 		data;
-	struct t_node			*pLeft;
-	struct t_node			*pRight;	
+	DATA_TYPE 				data;		// data
+	int 					bf;			// average element
+	struct t_node			*pLeft;		// left child
+	struct t_node			*pRight;	// right child
 }T_NODE, *P_NODE;
+
+
+T_NODE* node_create(DATA_TYPE data)
+{
+	T_NODE *pNewNode = malloc(sizeof(T_NODE));
+	pNewNode->data = data;
+	pNewNode->bf = 0;
+	pNewNode->pLeft = NULL;
+	pNewNode->pRight = NULL;
+	return pNewNode;
+}
+
+void node_free( T_NODE **pNode )
+{
+	if( NULL != pNode && *pNode != NULL )
+	{
+		free(*pNode);
+		*pNode = NULL;
+	}
+}
 
 void print_data( DATA_TYPE *pData)
 {
@@ -45,6 +74,17 @@ void back_display( T_NODE *pRoot )
 	back_display( pRoot->pRight );
 	print_data( &(pRoot->data));
 }
+
+// case： 左左 右右 左右 右左
+T_NODE* insert_data( T_NODE *pRoot,  DATA_TYPE data)
+{
+	if( NULL == pRoot )
+	{
+		return NULL;
+	}
+	
+}
+
 
 int main(int argc, char **argv)
 {
