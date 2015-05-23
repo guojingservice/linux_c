d83ac37cc0abc5a5a8958c08cd0a709e690e2a47
guojing013214@163.com <guojing013214@163.com>
2015-05-23 16:16:58 +0800

    prepare to implement avl

--- btree/avl/avl.c
+++ btree/avl/avl.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+typedef int DATA_TYPE;
+
+struct t_node{
+	DATA_TYPE 		data;
+	struct t_node 		*pLeft;
+	struct t_node 		*pRight;	
+}T_NODE;
+
 int main(int argc, char **argv)
 {
 	
