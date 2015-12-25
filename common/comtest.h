143f74b1a98c8b433aee42e07e28182506d6d675
vagrant <vagrant@centos6.(none)>
2015-12-25 09:18:31 +0100

    test k list

--- common/comtest.h
+++ common/comtest.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dlist.h"
-
+#include "klist.h"
 
 void comTestList()
 {
@@ -31,6 +32,66 @@ void comTestList()
     printf("head value of the list : %p\n", pList->head->value);
     printf("tail value of the list : %p\n", pList->tail->value);
 }
+
+void comTestKlist()
+{
+    typedef struct stu{
+        char name[64];
+        int num;
+        struct list_head list;
+    }Stu;
+    
+    static LIST_HEAD(stu_list); // init list head
+    char *stuNames[5] = {"Tom", "Kattom", "Tony", "Matheral","Ted"};
+    int i;
+    for( i = 0; i < sizeof(stuNames)/sizeof(stuNames[0]); ++i)
+    {
+        // init node
+        Stu *pNew = malloc(sizeof(struct stu));
+        if(!pNew)
+            exit(1);
+        sprintf(pNew->name,"%s", stuNames[i]);
+        pNew->num = i;
+         
+        list_add_tail(&pNew->list, &stu_list);
+    }
+    
+    
+    // iterat list, use list_for_each
+    struct list_head *cur;
+    Stu *pStu = NULL;
+    /*
+    list_for_each(cur, &stu_list)
+    {
+        pStu = list_entry(cur, Stu, list);     
+        printf("Name :%s, Num:%d, Stu Address:%p\n", 
+                pStu->name, 
+                pStu->num,
+                pStu);
+    }
+    */
+
+    // iterate list, use list_for_each_entry
+    list_for_each_entry(pStu, &stu_list, list)
+    {
+        printf("Name :%s, Num:%d, Stu Address:%p, NodeAddress:%p\n",
+                pStu->name,
+                pStu->num,
+                pStu,
+                &pStu->list);
+    }
+    printf("head prev:%p, head next:%p\n", pStu->list.prev, pStu->list.next);// still can access the invlid member address
+    printf("End pStu :%p, Danger Name:%s, DangerNum:%d\n", 
+            pStu,
+            pStu->name,
+            pStu->num);
+    //free(pStu); this will cause invalid member address access
+    //
+    
+    
+     
+}
+
 
 
 
