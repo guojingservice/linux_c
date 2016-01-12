d976a59d485e55fcee61009b04d42c6f85c97203
guojing <guojing013214@163.com>
2016-01-12 12:12:00 +0100

    a little define

--- common/mempool.c
+++ common/mempool.c
@@ -1,1 +1,146 @@
-#
+#include "mempool.h"
+
+
+unsigned int __mempool_calc_size(unsigned int max, unsigned int unit){
+    unsigned int size;
+    size = MEMPOOL_CALC(max, unit);
+    return size;
+}
+
+int mempool_init(struct mem_pool **ppmempool,
+                 unsigned int max,
+                 unsigned int unit,
+                 void *pbase,
+                 unsigned int size){
+    
+
+}
+
+void __mempool_init_head(struct mem_pool *pmempool,
+                         unsigned int max,
+                         unsigned int unit,
+                         unsigned int size){
+    int i;
+    char *padding;
+    
+    assert(NULL != pmempool);
+    
+    pmempool->magic = MEMPOOL_MAGIC;
+    pmempool->build = MEMPOOL_BUILD; 
+    
+    pmempool->max = max;
+    pmempool->unit = unit;
+    pmempool->real_unit = MEMPOOL_USER_DATA_UNIT_SIZE(unit);
+    
+    pmempool->size = MEMPOOL_CALC(max, unit);
+    pmempool->real_size = size;
+    
+    pmempool->used = 0;
+
+
+    pmempool->start = 0;
+    pmempool->end = 0;
+
+
+    pmempool->free_head = -1;
+    pmempool->free_tail = -1;
+
+    pmempool->used_head = -1;
+    
+    pmempool->last_cursor = -1L;
+    pmempool->iterator_last_cursor = -1L;
+    pmempool->is_iterator_reset = 0;
+    
+    pmempool->method = MEMPOOL_FIFO;
+    
+    pmempool->block_off = offsetof(struct mem_pool, blocks);
+    pmempool->block_size = MEMPOOL_BLOCK_SIZE(max);
+    
+    pmempool->data_off = MEMPOOL_INNER_DATA_SIZE(max);
+    pmempool->data_size = MEMPOOL_DATA_SIZE(max, unit);
+    
+    pmempool->use_protect = 0;
+    pmempool->protect_len = MEMPOOL_INNER_DATA_SIZE(max);
+
+    
+    padding = (char *) pmempool + MEMPOOL_INNER_DATA_BASE_SIZE(max);
+
+    for( i = 0; i < (pmempool->data_off - MEMPOOL_INNER_DATA_BASE_SIZE(max)); ++i){
+        *padding++ = i;
+    }
+    
+    return;
+}
+
+void __mempool_init_body(struct mem_pool *pmempool){
+    struct mem_block *ptail;
+    struct mem_block *pblock;
+    
+    assert( NULL != pmempool);
+    
+    int i;
+    
+    if(pmempool->max <= 0 )
+        return;
+    
+    pmempool->free_head = 0;
+    pmempool->free_tail = 0;
+
+    ptail = MEMPOOL_GET_PTR(pmempool, pmempool->free_tail);
+
+    ptail->next = -1;
+    ptail->valid = 0;
+    memset(&ptail->res, 0, sizeof(ptail->res));
+    ptail->index = 0;
+    ptail->prev = -1;
+
+    for( i = 1; i < pmempool->max; ++i){
+        ptail = MEMPOOL_GET_PTR(pmempool, pmempool->free_tail);
+        pblock = MEMPOOL_GET_PTR(pmempool, i);
+    
+        pblock->valid = 0;
+        memset(&pblock->res, 0, sizeof(pblock->res);
+        pblock->index = 0;
+        pblock->next = -1;
+        pblock->prev = i - 1;
+        ptail->next = i;
+        pmempool->free_tail = i;
+    } 
+    return;
+
+}
+
+int mempool_new(struct mem_pool **ppmempool,
+                unsigned int max, unsigned int unit){
+    unsigned int size = 0;
+    if( (0 >= max) || (0 >= unit))
+        return -1;
+    
+    size = MEMPOOL_CALC(max, unit);
+    
+    if(size / unit < max)
+        return -1;
+
+
+    if(!ppmempool)
+        return size;
+    //calloc will set every bit to 0
+    *ppmempool = (struct mem_pool *) calloc(1, size);
+    
+    if(!*ppmempool)
+        return -1;
+
+    // init head
+    __mempool_init_head(*ppmempool, max, unit, size); 
+
+    // init body
+    __mempool_init_body(*ppmempool);    
+
+    (*ppmempool)->is_calloc = 1;
+    (*ppmempool)->inited = 1;
+
+    return 0;
+
+}
+
+
--- common/mempool.h
+++ common/mempool.h
@@ -1,6 +1,178 @@
 /*
  * implement a memory pool
- *
+ * 
  *
  *
  */
+#ifndef _MEMPOOL_H
+#define _MEMPOOL_H
+
+#include "util.h"
+
+#define MEMPOOL_MAGIC 0x1234
+#define MEMPOOL_BUILD 2
+#define MEMPOOL_FIFO 0x00   // first in first out
+#define MEMPOOL_LIFO 0x01   // last in first out
+
+
+#define MEMPOOL_PAGE_SIZE 4096 // page size
+#define MEMPOOL_USER_DATA_ALIGN 8 //user data align bytes
+
+struct mem_block{
+    int valid;
+    int index;
+    int prev;
+    int next;
+    int res;
+};
+
+#define MEMBLOCK_IS_VALID(p) ((p)->valid)
+
+struct mem_pool{
+    int magic; // check mempool
+    int build; // check mempool
+    int max;   // size of mempool
+    int free_head;  // free list head;
+    int free_tail; // free list tail
+    int used_head; // used list head
+    int used;       // size of used memblock
+    int last_index; //last allocate index of memblock
+    int inited;     // if inited
+    int is_calloc; // if mempool is allocated by alloc 
+    int method;     // MEMPOOL_FIFO or MEMPOOL_LIFO
+    
+    volatile int start; // used for check mempool_alloc and mempool_free 'satomic nature
+    volatile int end;
+    
+    int unit;   // size of app data
+    int real_unit; // after roundup size
+    int size; // size of mempool
+    int real_size; // real size of mempool,equals to size
+    
+    int block_off; // offset of memblock area
+    int block_size; // size of memblock area
+    
+    int data_off; // offset of mem data area
+    int data_size; // size of memdata area
+
+    intptr_t last_cursor;
+    intptr_t iterator_last_cursor;
+    int is_iterator_reset;    
+
+    int use_protect;
+    int protect_len;
+    
+    char res[64]; // preserve;
+    
+    // start of the memblock area and data area
+    struct memblock blocks[1];
+
+};
+
+/*
+ * some frequently used macro defined here
+ *
+ */
+
+// make unit can be divisible up to round 
+#define MEMPOOL_ROUND(unit, round) ( ((intptr_t)(unit) + (intptr_t)(round) -1) / (intptr_t)(round) * (intptr_t)(round) )
+
+
+#define MEMPOOL_USER_DATA_UNIT_SIZE(size) MEMPOOL_ROUND(size, MEMPOOL_USER_DATA_ALIGN)
+
+#define MEMPOOL_DATA_SIZE(max, size) (max * MEMPOOL_USER_DATA_UNIT_SIZE(size))
+
+
+#define MEMPOOL_HEAD_SIZE (offsetof(struct mem_pool, blocks))
+
+#define MEMPOOL_BLOCK_SIZE(max) (max * sizeof(struct mem_block)) 
+
+#define MEMPOOL_INNER_DATA_BASE_SIZE(max) (MEMPOOL_HEAD_SIZE + MEMPOOL_BLOCK_SIZE(max))
+
+//alia to page size of linux
+#define MEMPOOL_INNER_DATA_SIZE(max) MEMPOOL_ROUND(MEMPOOL_INNER_DATA_BASE_SIZE(max),MEMPOOL_PAGE_SIZE)
+
+// total size of mempool
+#define MEMPOOL_CALC(max, unit) (MEMPOOL_INNER_DATA_SIZE(max) + MEMPOOL_DATA_SIZE(unit))
+
+
+#define MEMPOOL_GET_PTR(pstpool, idx) ((struct mem_block *)((pstpool)->blocks + ((size_t)(idx) % (pstpool->max)))) 
+
+// calculate the size of mem needed by mempool
+/*
+ * @ max: capacity of mempool
+ * @ unit: size of app data
+ * @ return: the size of memory required by mempool
+ */
+
+unsigned int __mempool_calc_size(unsigned int max, unsigned int unit);
+
+
+// init mempool memory, form free list
+/*
+ * @ppmempool: pp of mempool
+ * @max: capacity of mempool
+ * @unit: size of app data
+ * @pbase: base pointer of mempool
+ * @size: size of pbase point to
+ *
+ * @return: 0 success 
+ *          -1 fail
+ */
+int mempool_init(struct mempool **ppmempool, unsigned int max, 
+                 unsigned int unit,
+                 void *pbase,
+                 unsigned int size);
+
+
+void __mempool_init_head(struct mempool *pmempool,
+                         unsigned int max,
+                         unsigned int unit,
+                         unsigned int size);
+
+void __mempool_init_body(struct mempool *pmempool);
+
+int __mempool_check_head(struct mempool *pmempool,
+                         unsigned int max,
+                         unsigned int unit,
+                         unsigned int size);
+
+int __mempool_check_chain(struct mempool *pmempool)
+
+
+/* allocate mempool memory and init
+ *
+ * @ppmempool: pp to mempool
+ * @max: capacity of mempool
+ * @unit: size of app data
+ *
+ * @return: 0 success
+ *          -1 fail
+ */ 
+int mempool_new(struct mempool **ppmempool,
+                unsigned int max,
+                unsigned int unit);
+
+
+// TODO
+// attach a existed memory,check the data
+// mempool_attach
+//
+
+
+/* destroy mempool created by mempool_new
+ *
+ * @ppmempool: pp to mempool
+ *
+ * @return: 0 success
+ *          -1 fail
+ *
+ */
+int mempool_destroy(struct mempool **ppmempool);
+
+
+
+
+
+
+#endif
