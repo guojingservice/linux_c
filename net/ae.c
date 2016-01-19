f67c19ce35ec30694b823e548a723bc160083039
guojing <guojing013214@163.com>
2016-01-19 04:53:39 +0100

    do the rest

--- net/ae.c
+++ net/ae.c
@@ -225,6 +225,174 @@ static int processTimeEvents(aeEventLoop *eventLoop){
     aeTimeEvent *te;
     long long maxId;
     time_t now = time(NULL);
+    //reset lasttime in case of chaos
+    if( now < eventLoop->lastTime){
+        te = eventLoop->timeEventHead;
+        while(te){
+            te->when_sec = 0;
+            te = te->next;
+        }
+    }    
     
+    // update lastTIme
+    eventLoop->lastTime = now;
+    
+    // foreach time event list 
+    // process time event handler
+    te = eventLoop->timeEventHead;
+    maxId = eventLoop->timeEventNextId - 1;
+    while(te){
+        long now_sec, now_ms;
+        long long id;
+        // skip invalid events
+        // in fact, id which is greater then maxId may be add in the 
+        // time event handler process, to avoid loop forever,wo need this
+        // maxID to help us to control our range in a limit
+        if(te->id > maxId){
+            te = te->next;
+            continue;
+        }
+        
+        // get current time
+        aeGetTime(&now_sec, &now_ms);
+        // if now greater or equals to event exe time, then execute it
+        if(now_sec > te->when_sec ||
+           (now_sec == te->when_sec && now_ms >= te->when_ms)){
+            int retval;
+            id = te->id;
+            retval = te->timeProc(eventLoop, id, te->clientData);
+            processed++;
+
+            // record if to cycle this time event
+            if(retval != AE_NOMORE){
+                aeAddMillisecondsToNow(retval, &te->when_sec, &te->when_ms);            }else{
+                aeDeleteTimeEvent(eventLoop, id);
+            }
+            
+            // time event list may be changed after some event is executed
+            te = eventLoop->timeEventHead;
+
+
+            }
+        }else{
+            te = te->next;
+        }
+    }
     return processed;
+}
+
+//process event depending on flags parameter
+int aeProcessEvents(aeEventLoop *eventLoop, int flags){
+    int processed = 0, numevents;
+    // nothing to to
+    if(!(flags&AE_TIME_EVENTS) && !(flags & AE_FILE_EVENTS))
+        return 0;
+    
+    if(eventLoop->maxfd != -1 ||
+        ((flags & AE_TIME_EVENTS) && !(flags & AE_DONT_WAIT))){
+        int j;
+        aeTimeEvent *shortest = NULL;
+        struct timeval tv, *tvp;
+        // get recent time event
+        if(flags & AE_TIME_EVENTS &&
+            !(flags & AE_DONT_WAIT))
+            shortest = aeSearchNearestTimer(eventLoop);
+        if(shortest){
+            // calc the tmie missing for the nearest
+            aeGetTime(&now_sec, &now_ms);
+            tvp = &tv;
+            tvp->tv_sec = shortest->when-sec - now_sec;
+            if(shortest->when_ms < now_ms){
+                tvp->tv_usec = ((shortest->when_ms+1000) - now_ms)*1000;
+                tvp->tv_sec --;
+
+            }else{
+                tvp->tv_usec = (shortest->when_ms- now_ms)*1000;
+            }
+            if(tvp->tv_sec < 0) tvp->tv_sec = 0;
+            if(tvp->tv_usec < 0) tvp->tv_usec= 0;
+            
+        }
+    }else{
+        // there is no time event
+        // depend on FLAG, decided to whether block
+        if(flags & AE_DONT_WAIT){
+            tv.tv_sec = tv.tv_usec = 0;
+            tvp = &tv;
+        }
+        else{
+            tvp = NULL;
+        }
+    }
+    
+    // handle file event, block time is depended on tvp
+    numevents = aeApiPoll(eventLoop, tvp);
+    for(j = 0; j < numevents; ++j){
+        //pick up fired event
+        aeFileEvent *fe = &eventLoop->events[eventLoop->fired[j].fd];
+        int mask = eventLoop->fired[j].mask;
+        int fd = eventLoop->fired[j].fd;
+        int rfired = 0;
+        
+        if(fe->mask & mask & AE_READABLE){
+        // ifired is to make sure only one of read or write event will be executed
+            rfired = 1;
+            fe->rfileProc(eventLoop, fd, fe->clientData, mask);
+        }
+        if(fe->mask & mask & AE_WRITABLE){
+            if(!rfired || fe->wfileProc != fe->rfileProc)
+                fe->wfileProc(eventLoop, fd, fe->clientData, mask);
+        }
+        processed++;
+    }
+    // check time events
+    if(flags & AE_TIME_EVENTS)
+        processed += processTimeEvents(eventLoop);
+    return processed;
+
+}
+
+
+// wait for fd become readable , writable or exception occured
+int aeWait(int fd, int mask, long long milliseconds){
+    struct pollfd pfd;
+    int retmask = 0; retval;
+    memset(&pfd, 0, sizeof(pfd));
+    
+    pfd.fd = fd;
+    if(mask & AE_READABLE)pfd.events |= POLLIN;
+    if(mask & AE_WRITABLE)pfd.events |= POLLOUT;
+    if((retval = poll(&pfd, 1, milliseconds)) == 1){
+        if(pfd.revents & POLLIN) retmask |= AE_READABLE;
+        if(pfd.revents & POLLOUT) retmask |= AE_WRITABLE;
+        if(pfd.revents & POLLERR) retmask |= AE_WRITABLE;
+        if(pfd.revents & POLLHUP) retmask |= AE_WRITABLE;
+        return retmask;
+    }
+    else{
+        return retval;
+    }
+}
+
+// main loop of event handler
+void aeMain(aeEventLoop *eventLoop){
+    eventLoop->stop = 0;
+    while(!eventLoop->stop){
+        // if there is sth needed executed before event process,then do it
+        if(eventLoop->beforesleep != NULL){
+            eventLoop->beforesleep(eventLoop);
+        }
+        // start event process
+        aeProcessEvents(eventLoop, AE_ALL_EVENTS);
+    }
+}
+
+// return the name 
+char *aeGetApiName(void){
+    return aeApiName();
+}
+
+// set hander needed do before event handler
+void aeSetBeforeSleepProc(aeEventLoop *eventLoop, aeBeforeSleepProc *beforesleep){
+    eventLoop->beforesleep = beforesleep;
 }
