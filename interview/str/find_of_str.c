52bdb01eae98435bd974f65c9a4127d9efd8e2a5
katttom <guojing013214@163.com>
2016-06-02 23:20:15 +0800

    find brother str index

--- interview/str/find_of_str.c
+++ interview/str/find_of_str.c
@@ -57,16 +57,43 @@ int str_contain_bit(char *str1, int len1, char *str2, int len2)
     return 0;
 } 
 
+//寻找兄弟串: abc 和 cba 以及 cab为兄弟串    
+int str_find_brother(char *str1, int len1, char *str2, int len2)
+{
+    long mapbit = 0;
+    long trybit = 0;
+    int i =0;
+    int j =0;
+    for(i=0; i<len2;++i)
+    {
+        mapbit = mapbit | (1 << (str2[i] - 'A'));
+    }
+    for(i = 0; i < len1 - len2 +1; ++i)
+    {
+        trybit = 0;
+        for(j=i;j<i+len2; ++j)
+        {
+            trybit |= (1 <<(str1[j] - 'A'));
+        }
+        if((trybit ^ mapbit) == 0 )
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
 
 void test()
 {
     char temp1[128];
     char temp2[128];
-    snprintf(temp1, sizeof(temp1)-1,  "%s", "FABCDDGREGRNBRW");
-    snprintf(temp2, sizeof(temp2)-1,  "%s", "BADFGRBN");
+    snprintf(temp1, sizeof(temp1)-1,  "%s", "FABCDDGREGABDRNBRW");
+    snprintf(temp2, sizeof(temp2)-1,  "%s", "BAD");
     //printf("%d\n", str_contain(temp1, strlen(temp1), temp2, strlen(temp2)));
-    printf("%d\n", str_contain_bit(temp1, strlen(temp1), temp2, strlen(temp2)));
+    //printf("%d\n", str_contain_bit(temp1, strlen(temp1), temp2, strlen(temp2)));
     
+    printf("brother index : %d\n", str_find_brother(temp1,strlen(temp1), temp2, strlen(temp2)));    
 }
 //97 122 65 90
 int main()
